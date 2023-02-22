a81c12233ebb74b673dd0b1a84c5d077ee050cea
svetla.g <svetlana.georgieva@edu1.pgzaimov.com>
2023-02-22 11:25:27 +0200

    first commit

---
+++ ZadComparatori/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ZadComparatori
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
